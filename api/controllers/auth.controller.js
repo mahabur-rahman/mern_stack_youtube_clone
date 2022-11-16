123fb4c235a4879d5c33359117d10d769aa902a5
mahabur  rahman <annur4395@gmail.com>
2022-11-16 19:03:17 +0600

    google authenticatino

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -42,3 +42,36 @@ export const login = async (req, res, next) => {
     next(err);
   }
 };
+
+// GOOGLE AUTH
+
+export const googleAuth = async (req, res, next) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT);
+
+      return res
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .json(user._doc);
+    } else {
+      const newUser = new UserModel({ ...req.body, fromGoogle: true });
+      const savedUser = await newUser.save();
+
+      const token = jwt.sign({ id: savedUser._id }, process.env.JWT);
+
+      return res
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .json(savedUser._doc);
+    }
+  } catch (err) {
+    next(err);
+  }
+};
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { signUp, login } from "../controllers/auth.controller.js";
+import { signUp, login, googleAuth } from "../controllers/auth.controller.js";
 
 // ###############
 
@@ -12,8 +12,12 @@ import { signUp, login } from "../controllers/auth.controller.js";
 
 // CREATE USER
 router.post("/register", signUp);
+
 // SIGN IN USER
 router.post("/login", login);
+
+// GOOGLE AUTH
+router.post("/google", googleAuth);
 
 // export
 export default router;
