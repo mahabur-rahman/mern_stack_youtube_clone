f560020007032f49e7c9d5199e7d3ac15cc4e625
mahabur  rahman <annur4395@gmail.com>
2022-11-25 21:19:07 +0600

    upload video or image in firebase store

--- client/src/components/Upload.js
+++ client/src/components/Upload.js
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+
+import app from "../firebase";
+import axios from "axios";
 
 const Container = styled.div`
   width: 100%;
@@ -11,12 +21,13 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  z-index: 99;
 `;
 
 const Wrapper = styled.div`
   width: 600px;
   height: 600px;
-  background: ${({ theme }) => theme.bgLighter};
+  background-color: ${({ theme }) => theme.bgLighter};
   color: ${({ theme }) => theme.text};
   padding: 20px;
   display: flex;
@@ -24,18 +35,15 @@ const Wrapper = styled.div`
   gap: 20px;
   position: relative;
 `;
-
 const Close = styled.div`
   position: absolute;
   top: 10px;
   right: 10px;
   cursor: pointer;
 `;
-
 const Title = styled.h1`
   text-align: center;
 `;
-
 const Input = styled.input`
   border: 1px solid ${({ theme }) => theme.soft};
   color: ${({ theme }) => theme.text};
@@ -44,11 +52,6 @@ const Input = styled.input`
   background-color: transparent;
   z-index: 999;
 `;
-
-const Label = styled.label`
-  font-size: 14px;
-`;
-
 const Desc = styled.textarea`
   border: 1px solid ${({ theme }) => theme.soft};
   color: ${({ theme }) => theme.text};
@@ -65,30 +68,148 @@ const Button = styled.button`
   background-color: ${({ theme }) => theme.soft};
   color: ${({ theme }) => theme.textSoft};
 `;
+const Label = styled.label`
+  font-size: 14px;
+`;
 
-export default function Upload({ setOpen }) {
+function Upload({ setOpen }) {
+  const [img, setImg] = useState(undefined); // img
+  const [video, setVideo] = useState(undefined); // video
+  const [inputs, setInputs] = useState({}); // title, desc
+  const [tags, setTags] = useState([]); // tags
+
+  //   percentage show
+  const [imgPerc, setImgPerc] = useState(0);
+  const [videoPerc, setVideoPerc] = useState(0);
+
+  const navigate = useNavigate();
+
+  //   handleChange  | title, desc
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  //   handleTags | tags
+  const handleTags = (e) => {
+    setTags(e.target.value.split(","));
+  };
+
+  //   uploadFile
+  const uploadFile = (file, urlType) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        urlType === "imgUrl"
+          ? setImgPerc(Math.round(progress))
+          : setVideoPerc(Math.round(progress));
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+          default:
+            break;
+        }
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setInputs((prev) => {
+            return { ...prev, [urlType]: downloadURL };
+          });
+        });
+      }
+    );
+  };
+
+  //   for img
+
+  useEffect(() => {
+    img && uploadFile(img, "imgUrl");
+  }, [img]);
+
+  // for video
+
+  useEffect(() => {
+    video && uploadFile(video, "videoUrl");
+  }, [video]);
+
+  //   handleUpload button clicked || finally data post on mongoDB 👍
+
+  const handleUpload = async (e) => {
+    e.preventDefault();
+
+    const res = await axios.post("/videos", { ...inputs, tags });
+
+    setOpen(false);
+
+    res.status === 200 && navigate(`/video/${res.data._id}`);
+  };
+
   return (
-    <>
-      <Container>
-        <Wrapper>
-          <Close onClick={() => setOpen(false)}>X</Close>
-          <Title>Upload a new video</Title>
+    <Container>
+      <Wrapper>
+        <Close onClick={() => setOpen(false)}>X</Close>
+        <Title>Upload a New Video</Title>
+        <Label>Video:</Label>
 
-          <Label>Video:</Label>
-          <Input type="file" accept="video/*" />
+        {videoPerc > 0 ? (
+          "Uploading video :" + videoPerc + " %"
+        ) : (
+          <Input
+            type="file"
+            accept="video/*"
+            onChange={(e) => setVideo(e.target.files[0])}
+          />
+        )}
 
-          <Input type="text" placeholder="title" />
+        <Input
+          type="text"
+          placeholder="Title"
+          name="title"
+          onChange={handleChange}
+        />
+        <Desc
+          placeholder="Description"
+          name="desc"
+          rows={8}
+          onChange={handleChange}
+        />
+        <Input
+          type="text"
+          placeholder="Separate the tags with commas."
+          onChange={handleTags}
+        />
 
-          <Desc placeholder="description.." rows={8} />
+        <Label>Image:</Label>
 
-          <Input type="text" placeholder="separate the tags with commas.." />
+        {imgPerc > 0 ? (
+          "Uploading Image " + imgPerc + " %"
+        ) : (
+          <Input
+            type="file"
+            accept="image/*"
+            onChange={(e) => setImg(e.target.files[0])}
+          />
+        )}
 
-          <Label>Image:</Label>
-          <Input type="file" accept="image/*" />
-
-          <Button>Upload</Button>
-        </Wrapper>
-      </Container>
-    </>
+        <Button onClick={handleUpload}>Upload</Button>
+      </Wrapper>
+    </Container>
   );
 }
+
+export default Upload;
